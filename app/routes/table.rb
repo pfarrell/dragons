198c4bba9f52ca2fa0668b14380d7bfd926281ea
Patrick Farrell <mr.patfarrell@gmail.com>
2015-03-04 14:50:02 -0800

    converted index list to lambda driven table

--- app/routes/table.rb
+++ app/routes/table.rb
@@ -1,13 +1,24 @@
 class App < Sinatra::Application
-
-  get "/table/:table_name" do
-    table = session[:db].table(params[:table_name])
+  def table_header
     props={}
     props["field"]={value: lambda{|x| x[0]}}
     props["type"]={value: lambda{|x| x[1][:db_type]}}
     props["primary key?"]={value: lambda{|x| x[1][:primary_key]}}
+    props
+  end
+
+  def index_header
+    props={}
+    props["name"]={value: lambda{|x| x[0]}}
+    props["columns"]={value: lambda{|x| x[1][:columns].join(', ')}}
+    props
+  end
+
+  get "/table/:table_name" do
+    table = session[:db].table(params[:table_name])
     haml :table, locals: {
-      header: props,
+      table_struct: {header:table_header, data:table.columns},
+      index_struct: {header:index_header, data:table.indexes},
       table: table
     }
   end
