c39d2b16fad478cf50135fdf1c0d4fb783aeab50
Patrick Farrell <mr.patfarrell@gmail.com>
2015-03-04 11:10:23 -0800

    moved columns to own route and added view for single column

---
+++ app/routes/column.rb
@@ -0,0 +1,15 @@
+class App < Sinatra::Application
+  def column_hash
+    summary = Hash.new{|hash, key| hash[key] = Array.new;}
+    session[:db].columns.map{|x| summary[x[:column_name]] << x[:table_name]}
+    summary
+  end
+
+  get "/columns" do
+    haml :columns, locals: { columns: column_hash}
+  end
+
+  get "/column/:column_name" do
+    haml :column, locals: {column: column_hash[params[:column_name]]}
+  end
+end
