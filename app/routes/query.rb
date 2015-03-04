f450af6a47e1d54265094b1d2368d74fe1f0a36a
Patrick Farrell <mr.patfarrell@gmail.com>
2015-03-03 22:36:26 -0800

    display query error messages on query page

--- app/routes/query.rb
+++ app/routes/query.rb
@@ -1,9 +1,13 @@
 class App < Sinatra::Application
   get "/query" do 
-    haml :query, locals: { query: nil, results: nil }
+    haml :query, locals: { query: nil, results: nil, error: nil }
   end
 
   post "/query" do
-    haml :query, locals: { query: params[:query], results: session[:db].run_query(params[:query]).all }
+    begin
+    haml :query, locals: { query: params[:query], results: session[:db].run_query(params[:query]).all, error: nil }
+    rescue Exception => ex
+      haml:query, locals: {query: params[:query], results: nil, error: ex.message} 
+    end
   end
 end
