fa8c52cc912902053d130d3827028c1bf1188ed6
Patrick Farrell <mr.patfarrell@gmail.com>
2015-03-09 11:49:10 -0700

    change search to a GET request for bookmarking capabilities

--- app/routes/search.rb
+++ app/routes/search.rb
@@ -1,14 +1,12 @@
 class App < Sinatra::Application
-  post "/search" do
-    ret={}
-    ret[:tables] = Table.search(session[:db], params[:query]).sort
-    ret[:views] = View.search(session[:db], params[:query]).sort
-    ret[:routines] = Routine.search(session[:db], params[:query]).sort
-    ret[:columns] = Column.search(session[:db], params[:query])
-    haml :search, locals: {query: params[:query], results: ret}
-  end
-
   get "/search" do
-    haml :search, locals: {query: nil, results: []}
+    ret={}
+    unless params[:query].nil?
+      ret[:tables] = Table.search(session[:db], params[:query]).sort
+      ret[:views] = View.search(session[:db], params[:query]).sort
+      ret[:routines] = Routine.search(session[:db], params[:query]).sort
+      ret[:columns] = Column.search(session[:db], params[:query])
+    end
+    haml :search, locals: {query: params[:query], results: ret}
   end
 end
