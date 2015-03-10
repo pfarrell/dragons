03586bb204d5ea69e85933050acd6e540931fac0
Patrick Farrell <mr.patfarrell@gmail.com>
2015-03-10 11:00:17 -0700

    column lookups are now case insensitive

--- app/routes/column.rb
+++ app/routes/column.rb
@@ -1,7 +1,7 @@
 class App < Sinatra::Application
   def column_hash
     summary = Hash.new{|hash, key| hash[key] = Array.new;}
-    Database[session[:db]].columns.map{|x| summary[x[:column_name]] << x[:table_name]}
+    Database[session[:db]].columns.map{|x| summary[x[:column_name].downcase] << x[:table_name]}
     summary
   end
 
@@ -14,6 +14,6 @@ class App < Sinatra::Application
   end
 
   get "/columns/:column_name" do
-    haml :column, locals: {column: column_hash[params[:column_name]]}
+    haml :column, locals: {column: column_hash[params[:column_name].downcase]}
   end
 end
