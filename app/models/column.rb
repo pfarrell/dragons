42cacfbf501a3ac461e7abffad5d36be28c54abe
Patrick Farrell <mr.patfarrell@gmail.com>
2015-04-09 08:00:59 -0700

    column search is now uniqified and sorted on display

--- app/models/column.rb
+++ app/models/column.rb
@@ -1,5 +1,5 @@
 class Column
   def self.search(conn, query)
-    conn.columns.all.select{|x| x[:column_name]=~/#{query}/i}
+    conn.columns.all.select{|x| x[:column_name]=~/#{query}/i}.uniq{|x| x[:column_name]}
   end
 end
--- app/routes/search.rb
+++ app/routes/search.rb
@@ -5,7 +5,7 @@ class App < Sinatra::Application
       ret[:tables] = Table.search(Database[session[:db]], params[:query]).sort
       ret[:views] = View.search(Database[session[:db]], params[:query]).sort
       ret[:routines] = Routine.search(Database[session[:db]], params[:query]).sort
-      ret[:columns] = Column.search(Database[session[:db]], params[:query])
+      ret[:columns] = Column.search(Database[session[:db]], params[:query]).sort_by{|col| col[:column_name]}
     end
     haml :search, locals: {query: params[:query], results: ret}
   end
