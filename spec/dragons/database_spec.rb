62052522c3ac08d5c28f2f3d68889a66b666593c
Patrick Farrell <mr.patfarrell@gmail.com>
2015-04-19 12:16:46 -0700

    test for mssql routines

--- spec/dragons/database_spec.rb
+++ spec/dragons/database_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe 'Database' do
   let(:db){ Database.new(connection: '{"adapter":"test", "host":"localhost", "database":"database"}')}
+  let(:db2){ Database.new(connection: '{"adapter":"tinytds", "host":"localhost", "database":"database"}')}
+
   it "can be instantiated with a json string" do
     expect(db.conn["adapter"]).to eq("test")
     expect(db.conn["host"]).to eq("localhost")
@@ -18,6 +20,10 @@ describe 'Database' do
  
   it "has a database method" do
     expect(db.database).to eq("database")
+  end
+
+  it "handles mssql routines" do
+    expect(db2.mssql_routine("tinytds", "dbo")).to be_a(Sequel::TinyTDS::Dataset)
   end
 
 end
