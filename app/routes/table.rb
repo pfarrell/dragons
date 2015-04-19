be756651601b40a9f93a6e91d3b2d38d02197915
Patrick Farrell <mr.patfarrell@gmail.com>
2015-04-19 11:37:44 -0700

    coverage for indexes and rename for function overwriting between routes

--- app/routes/table.rb
+++ app/routes/table.rb
@@ -7,20 +7,20 @@ class App < Sinatra::Application
     props
   end
 
-  def index_header
+  def table_index_header
     props={}
     props["name"]={value: lambda{|x| x[0]}}
     props["columns"]={value: lambda{|x| x[1][:columns].join(', ')}}
     props
   end
 
-  def routine_header
+  def table_routine_header
     props={}
     props["name"]={value: lambda{|x| x}, link: lambda{|x| "/routines/#{x}"}}
     props
   end
   
-  def foreign_key_header
+  def table_foreign_key_header
     props={}
     props["Name"]={value: lambda{|x| x[:name]}}
     props["Columns"]={value: lambda{|x| x[:columns].join(", ")}}
@@ -33,9 +33,9 @@ class App < Sinatra::Application
     table = Database[session[:db]].table(params[:table_name])
     haml :table, locals: {
       table_struct: {header:table_header, data:table.columns},
-      index_struct: {header:index_header, data:table.indexes.sort},
-      routine_struct: {header:routine_header, data:table.routines.sort},
-      foreign_key_struct: {header:foreign_key_header, data: table.foreign_keys},
+      index_struct: {header:table_index_header, data:table.indexes.sort},
+      routine_struct: {header:table_routine_header, data:table.routines.sort},
+      foreign_key_struct: {header:table_foreign_key_header, data: table.foreign_keys},
       table: table
     }
   end
--- app/routes/view.rb
+++ app/routes/view.rb
@@ -7,20 +7,20 @@ class App < Sinatra::Application
     props
   end
 
-  def index_header
+  def view_index_header
     props={}
     props["name"]={value: lambda{|x| x[0]}}
     props["columns"]={value: lambda{|x| x[1][:columns].join(', ')}}
     props
   end
 
-  def routine_header
+  def view_routine_header
     props={}
     props["name"]={value: lambda{|x| x}, link: lambda{|x| "/routines/#{x}"}}
     props
   end
   
-  def foreign_key_header
+  def view_foreign_key_header
     props={}
     props["Name"]={value: lambda{|x| x[:name]}}
     props["Columns"]={value: lambda{|x| x[:columns].join(", ")}}
@@ -45,9 +45,9 @@ class App < Sinatra::Application
     view = Database[session[:db]].view(params[:view_name])
     haml :view, locals: {
       view_struct: {header:view_header, data:view.columns},
-      index_struct: {header:index_header, data:view.indexes.sort},
-      routine_struct: {header:routine_header, data:view.routines.sort},
-      foreign_key_struct: {header:foreign_key_header, data: view.foreign_keys},
+      index_struct: {header:view_index_header, data:view.indexes.sort},
+      routine_struct: {header:view_routine_header, data:view.routines.sort},
+      foreign_key_struct: {header:view_foreign_key_header, data: view.foreign_keys},
       view: view
     }
   end
