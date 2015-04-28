f579f62b3855e7ac4a5797f8e33a2cb76022ce53
Patrick Farrell <mr.patfarrell@gmail.com>
2015-04-28 08:34:46 -0700

    connection input form

--- app/routes/connection.rb
+++ app/routes/connection.rb
@@ -1,6 +1,13 @@
 class App < Sinatra::Application
   get "/connections" do
-    haml :connection, locals:{model: Database.order(Sequel.desc(:last_used)).all}
+    haml :connections, locals:{model: Database.order(Sequel.desc(:last_used)).all}
+  end
+
+  post "/connections" do
+    conn = Database.new
+    conn.connection = params.select{|x| !x.nil? && x != ""}.to_json
+    conn.save
+    haml :connections, locals:{model: Database.order(Sequel.desc(:last_used)).all}
   end
 end
 
