a4d0b6a9d59d83028427cf614f453e4bc49cb9d0
Patrick Farrell <mr.patfarrell@gmail.com>
2015-05-29 18:09:21 -0700

    fixed notes spec to save note

--- spec/dragons/models/note_spec.rb
+++ spec/dragons/models/note_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe Note do
-  let(:note) { Note.new(note: "test note")}
+  let(:note) { Note.new(note: "test note").save}
   it "saves notes" do
     expect(note.id).to_not be_nil
   end
